2bd0191fab9cfd45c8910a373a0f6a321026849c
PhantomSean <sean.stewart@ucdconnect.ie>
2020-04-01 15:16:23 +0100

    Commit:  added template to GenerateSolution class

--- src/GenerateSolution.java
+++ src/GenerateSolution.java
@@ -1,2 +1,17 @@
+import Classes.Solution;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class GenerateSolution {
+    private static List<Solution> solutions = new ArrayList<>();
+
+    public static void  main(String[] args) {
+        genSolution();
+    }
+
+    public static void genSolution() {
+
+    }
+
 }
