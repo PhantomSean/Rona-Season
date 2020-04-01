f7ccbf763484d12ef82acee8d903999519820fcb
PhantomSean <sean.stewart@ucdconnect.ie>
2020-04-01 16:38:41 +0100

    Commit:  added giveRandomProject method to GenerateSolution

--- src/GenerateSolution.java
+++ src/GenerateSolution.java
@@ -7,19 +7,38 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Random;
 
 public class GenerateSolution {
     private static List<Solution> solutions = new ArrayList<>();
+    private static HashMap<String, Project> projects;
+    private static List<Student> students;
+    private static List<Staff> staff;
 
-    public static void  main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
         genSolution();
+        System.out.println(giveRandomProject().getTitle());
     }
 
     private static void genSolution() throws IOException {
-        HashMap<String, Project> projects = PopulateClasses.populateProjectClass("Staff&Projects(60).xlsx");
-        List<Student> students = PopulateClasses.populateStudentClass("Students&Preferences(60).xlsx");
-        List<Staff> staff = PopulateClasses.populateStaff("Staff&Projects(60).xlsx");
+        projects = PopulateClasses.populateProjectClass("Staff&Projects(60).xlsx");
+        students = PopulateClasses.populateStudentClass("Students&Preferences(60).xlsx");
+        staff = PopulateClasses.populateStaff("Staff&Projects(60).xlsx");
     }
 
+    private static Project giveRandomProject() {
+        Project project = genProject();
+        if (project.isTaken())
+            giveRandomProject();
+        project.setTaken(true);
+        return project;
+    }
+
+    private static Project genProject() {
+        Random generator = new Random();
+        Object[] values = projects.values().toArray();
+        Project randomProject = (Project) values[generator.nextInt(values.length)];
+        return randomProject;
+    }
 
 }
