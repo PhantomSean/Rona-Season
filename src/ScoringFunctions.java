1f107ca9a4cde0720f2a5c69fce2eaeb38e6b551
Oscar Byrne Carty <oscar.byrnecarty@ucdconnect.ie>
2020-04-09 15:38:51 +0100

    added comments and test suite for methods

--- src/ScoringFunctions.java
+++ src/ScoringFunctions.java
@@ -1,3 +1,4 @@
+import Classes.Project;
 import Classes.Solution;
 import Classes.Student;
 
@@ -13,9 +14,11 @@ public class ScoringFunctions {
         List<Solution> solutions = GenerateSolution.genSolution();
         change(solutions);
         analyze(solutions);
+        testSuite();
     }
 
     private static void change(List<Solution> solutions){
+        //making list for solutions which did not get a preference
         List<Solution> notGotPref = new ArrayList<>();
         List<Solution> temp = new ArrayList<>();
         for (Solution solution : solutions) {
@@ -23,21 +26,22 @@ public class ScoringFunctions {
                 notGotPref.add(solution);
             }
         }
-        for (int i = 0; i < solutions.size(); i++) {
-            if (solutions.get(i).getPrefGotten() != 0) {
-                temp.add(solutions.get(i));
+        //going through each student that got a preference
+        for (int i = 0; i < solutions.size(); i++) {                                                        //Takes studentA that got a preferential project and goes through
+            if (solutions.get(i).getPrefGotten() != 0) {                                                    //all the students whos project was not on their preference list
+                temp.add(solutions.get(i));                                                                 // but had studentA's project on their list
                 for(int j = 0; j < notGotPref.size(); j++){
                     if(checkForPref(solutions.get(j).getProjectTitle(), notGotPref.get(j).getStudent())){
                         temp.add(notGotPref.get(j));
                     }
                 }
-                if(temp.size() > 1) {
+                if(temp.size() > 1) {                                                                       //studentA's project is randomly assigned
                     Random r = new Random();
                     int random = r.nextInt(temp.size() - 1);
-                    if(random != 0) {
-                        temp.get(random).getStudent().setPrefGotten(getPrefNumber(temp.get(0).getProjectTitle(), temp.get(random).getStudent()));
-                        temp.get(random).getProject().setTaken(false);
-                        temp.get(0).getStudent().setPrefGotten(0);
+                    if(random != 0) {                                                                       //if the project is not randomly assigned to the student who already had it
+                        temp.get(random).getStudent().setPrefGotten(getPrefNumber(temp.get(0).getProjectTitle(), temp.get(random).getStudent()));//student gets a random project in their stream
+                        temp.get(random).getProject().setTaken(false);                                                                           //then a random student who had the project on their
+                        temp.get(0).getStudent().setPrefGotten(0);                                                                               //list gets it
                         solutions.get(returnNumber(solutions, temp.get(random))).setProject(temp.get(0).getProject());
                         solutions.get(returnNumber(solutions, temp.get(0))).setProject(GenerateSolution.giveRandomProject(temp.get(0).getStudent().getStream()));
                         solutions.get(returnNumber(solutions, temp.get(0))).getProject().setTaken(true);
@@ -50,6 +54,7 @@ public class ScoringFunctions {
 
     }
 
+    //method for checking if a student had a project in their preferences
     private static boolean checkForPref(String project, Student student){
         for(int i = 0; i < 10; i++){
             if(student.getPreference(i).equals(project)){
@@ -59,6 +64,7 @@ public class ScoringFunctions {
         return false;
     }
 
+    //method for checking what number the student had the preference at
     private static int getPrefNumber(String project, Student student){
         for(int i = 0; i < 10; i++){
             if(student.getPreference(i).equals(project)){
@@ -68,6 +74,7 @@ public class ScoringFunctions {
         return 0;
     }
 
+    //method which returns which number of the list a solution is on
     private static int returnNumber(List<Solution> solutions, Solution s){
         for(int i = 0; i < solutions.size(); i++){
             if(solutions.get(i).getProjectTitle().equals(s.getProjectTitle())){
@@ -77,7 +84,7 @@ public class ScoringFunctions {
         return 0;
     }
 
-
+    //method for analyzing a solution
     private static void analyze(List<Solution> solutions){
         int check = 0;
         for(int i = 0; i < 11; i++){
@@ -97,6 +104,74 @@ public class ScoringFunctions {
         for (Solution solution : solutions) {
             System.out.println(solution.getStudentName() + ": " + solution.getProjectTitle());
         }
+
+
     }
 
+//----------------------------------------------------------------------------------------------------------------------------------//
+    //TEST METHODS
+    private static String testCheckForPref(){
+        List<String> preferences = new ArrayList<String>();
+        preferences.add("test");
+        preferences.add("z");
+        preferences.add("x");
+        preferences.add("y");
+
+        Student student = new Student("check", "CS", 0, preferences, false, 0, 4.0);
+
+        if(checkForPref("test", student)){
+            return "checkForPref() method works";
+        }else{
+            return "error in method checkForPref()";
+        }
+    }
+
+    private static String testGetPrefNumber(){
+        List<String> preferences = new ArrayList<String>();
+        preferences.add("z");
+        preferences.add("test");
+        preferences.add("x");
+        preferences.add("y");
+
+        Student student = new Student("check", "CS", 0, preferences, false, 0, 4.0);
+
+        if(getPrefNumber("test", student) == 1){
+            return "getPrefNumber() method works";
+        }else{
+            return "error in method getPrefNumber()";
+        }
+    }
+
+    private static String testReturnNumber(){
+        List<Solution> solutions = new ArrayList<>();
+        List<String> preferences = new ArrayList<String>();
+
+        Student studentOne = new Student("check", "CS", 0, preferences, false, 0, 4.0);
+        Student studentTwo = new Student("chuck", "DS", 1, preferences, false, 0, 1.0);
+        Student studentThree = new Student("cheek", "CS", 2, preferences, false, 0, 2.0);
+        Student studentFour = new Student("cheese", "DS", 3, preferences, false, 0, 3.0);
+
+        Project project = new Project("x", "y", "z", false);
+
+        Solution two = new Solution(studentTwo, project, 0);
+        solutions.add(two);
+        solutions.add(new Solution(studentOne, project, 1));
+        solutions.add(new Solution(studentThree, project, 1));
+        solutions.add(new Solution(studentFour, project, 1));
+
+        if(returnNumber(solutions, two) == 0){
+            return "returnNumber() method works";
+        }else{
+            return "error in method returnNumber()";
+        }
+    }
+
+    private static void testSuite(){
+        System.out.println("\n");
+        System.out.println("Test Results:");
+        System.out.println(testCheckForPref());
+        System.out.println(testGetPrefNumber());
+        System.out.println(testReturnNumber());
+        System.out.println("\n");
+    }
 }
