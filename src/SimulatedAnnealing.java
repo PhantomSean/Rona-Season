934eec729da5806dcaf6f2b21e4f26f1444912b8
Oscar Byrne Carty <oscar.byrnecarty@ucdconnect.ie>
2020-04-17 17:55:23 +0100

    tweaked

--- src/SimulatedAnnealing.java
+++ src/SimulatedAnnealing.java
@@ -9,15 +9,25 @@ public class SimulatedAnnealing {
         simulatedAnnealing();
 
     }
-
     private static void simulatedAnnealing() throws IOException {
         int temperature = 100;
         List<Solution> solutions = GenerateSolution.genSolution(new ArrayList<>());
         ScoringFunctions.main(solutions);
+
         while(temperature > 0){
-            solutions = HillClimbing.acceptance(solutions, HillClimbing.change(solutions));
+            solutions = acceptance(solutions, HillClimbing.change(solutions));
             temperature -= 5;
         }
         ScoringFunctions.main(solutions);
+    }
+
+    // Decide whether or not to accept the new solution
+    private static List<Solution> acceptance(List<Solution> solutions, List<Solution> changedSolutions){
+
+        if(ScoringFunctions.scoreSolution(solutions) > ScoringFunctions.scoreSolution(changedSolutions)){
+            return changedSolutions;
+        }else{
+            return solutions;
+        }
     }
 }
