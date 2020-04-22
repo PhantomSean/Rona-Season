e8d14a6efad4f500e90c2da0c27e479b2cd806b1
caolanpowerUCD <caolan.power@ucdconnect.ie>
2020-04-22 17:02:34 +0100

    Mutate method added

--- src/GeneticAlgorithm.java
+++ src/GeneticAlgorithm.java
@@ -1,9 +1,12 @@
+import Classes.Project;
 import Classes.Solution;
+import Classes.Student;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 
 public class GeneticAlgorithm implements Solver{
 
@@ -55,6 +58,25 @@ public class GeneticAlgorithm implements Solver{
 		return population;
 	}
 
+
+	private static Project mutate(List<Solution> parent1, List<Solution> parent2, List<Project> projects){
+		List<Project> unassignedProjects = new ArrayList<>(projects);
+
+		for(int i=0; i<parent1.size(); i++){
+
+			if (projects.contains(parent1.get(i).getProject()))
+				unassignedProjects.remove(parent1.get(i).getProject());
+			if (projects.contains(parent2.get(i).getProject()))
+				unassignedProjects.remove(parent2.get(i).getProject());
+
+		}
+		Random rand = new Random();
+
+		projects.get(rand.nextInt(unassignedProjects.size()));
+
+		return unassignedProjects.get(rand.nextInt(unassignedProjects.size()));
+	}
+
 //----------------------------------------------------------------------------------------------------------------------------------//
 	//TEST METHODS
 
@@ -101,5 +123,35 @@ public class GeneticAlgorithm implements Solver{
 		}else{
 			return "error in method cullPopulation";
 		}
+	}
+
+	static String testMutate(){
+		List<Solution> parent1 = new ArrayList<>();
+		List<Solution> parent2 = new ArrayList<>();
+
+		Project one = new Project("One", "CS", "ME", false);
+		Project two = new Project("Two", "CS", "ME", false);
+		Project three = new Project("Three", "CS", "ME", false);
+		Project four = new Project("Four", "CS", "ME", false);
+		List<String> empty = new ArrayList<>();
+
+		Student me = new Student("ME", "CS", 0, empty, true, 0, 4.0);
+
+		parent1.add(new Solution(me, one, 0));
+		parent2.add(new Solution(me, two, 0));
+
+		List<Project> projects = new ArrayList<Project>();
+
+		projects.add(one);
+		projects.add(two);
+		projects.add(three);
+		projects.add(four);
+
+		Project test = mutate(parent1, parent2, projects);
+
+		if (test.getTitle().equals("Three") || test.getTitle().equals("Four"))
+			return "Mutate method working";
+		else
+			return "Mutate method not working";
 	}
 }
