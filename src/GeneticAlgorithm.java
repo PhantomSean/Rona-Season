27c5d6ecb060f261ece8366813d3af9e4fc110e9
PhantomSean <sean.stewart@ucdconnect.ie>
2020-05-15 16:01:37 +0100

    Commit: debugging

--- src/GeneticAlgorithm.java
+++ src/GeneticAlgorithm.java
@@ -139,7 +139,7 @@ public class GeneticAlgorithm implements Solver{
         for (ArrayList<Solution> solutionArrayList : population){
             System.out.println("---------------------------------------------------");
             for (Solution solution : solutionArrayList){
-                System.out.println(solution.getStudentName() + " : " + solution.getProjectTitle() + " : " + solution.getPrefGotten() + " : " + solution.getStudent().getPreference(solution.getPrefGotten()-1));
+                System.out.println(solution.getStudentName() + " : " + solution.getProjectTitle() + " : " + solution.getPrefGotten());
             }
         }
     }
